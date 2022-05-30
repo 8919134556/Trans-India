45db257c496e44bbc4eec32e6529920bef610396
suryaanand nadendla <103498529+8919134556@users.noreply.github.com>
2022-05-30 11:44:16 +0530

    Update settings.py

--- TransIndia/settings.py
+++ TransIndia/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p%npc#@)!pol)xt&5&3lr*_9()ix39k561#g5y3$yft!fmtmb2'
+SECRET_KEY = '********************************************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -150,8 +150,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'suryaanand975@gmail.com'
-EMAIL_HOST_PASSWORD = 'Anand@123'
+EMAIL_HOST_USER = '<your_email_addres>'
+EMAIL_HOST_PASSWORD = '<your_password>'
 EMAIL_USE_TLS = True
 
 BASE_COUNTRY = "UK"
